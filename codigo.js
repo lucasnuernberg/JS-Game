c728276cd9c2610f44e20e2162ee78529de769dd
lucasnuernberg <lucasnogoncalves@hotmail.com>
2020-10-28 13:02:40 -0300

    changes

--- codigo.js
+++ codigo.js
@@ -1,2 +1,17 @@
 var mainButtom = document.getElementById('selectCharacter')
 
+mainButtom.onclick = function() {
+    window.location.href = 'game.html'
+}
+
+var imageElement = document.getElementsByTagName('img')
+for (var x = 0; x < imageElement.length; x++) {
+    imageElement[x].onclick = function() {
+        var selecionada = x
+        console.log(selecionada)
+    }
+}
+console.log(selecionada)
+
+
+
